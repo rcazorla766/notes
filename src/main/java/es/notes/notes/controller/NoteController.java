42bc831639b70f7453de0caa3cc0a55f8b78ddbc
rcazorla766 <rcazorla766@gmail.com>
2025-10-29 19:31:36 +0100

    noteController

--- src/main/java/es/notes/notes/controller/NoteController.java
+++ src/main/java/es/notes/notes/controller/NoteController.java
@@ -1,6 +1,31 @@
 package es.notes.notes.controller;
 
+import es.notes.notes.model.Note;
+import es.notes.notes.service.NoteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/notas")
 public class NoteController {
+
+    private NoteService noteService;
+
+    @Autowired
+    public NoteController(NoteService noteService) {
+        this.noteService = noteService;
+    }
+
+    @GetMapping
+    public String obtenerTodas(){
+        List<Note> notes =noteService.obtenerTodas();
+        return "index";
+    }
+
+
 
     //TODO
 }
