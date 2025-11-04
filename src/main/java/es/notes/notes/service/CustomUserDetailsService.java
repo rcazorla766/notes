5ff2368fe3b0a051e40015d1bb7f1ece43a7a65d
rcazorla766 <rcazrola766@gmail.com>
2025-11-04 13:08:54 +0100

    CustomUserDetailService implemented

--- src/main/java/es/notes/notes/service/CustomUserDetailsService.java
+++ src/main/java/es/notes/notes/service/CustomUserDetailsService.java
@@ -1,4 +1,30 @@
 package es.notes.notes.service;
 
-public class CustomUserDetailsService {
+import es.notes.notes.model.AppUser;
+import es.notes.notes.repository.AppUserRepository;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service; 
+
+@Service
+public class CustomUserDetailsService extends AuthService {
+
+    private final AppUserRepository repo;
+
+    public CustomUserDetailsService(AppUserRepository repo) {
+        this.repo = repo;
+    }
+
+    @Overwride
+    public UserDetails loadUserByUsername(String username) {
+        AppUser appUser = repo.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        
+        return new org.springframework.security.core.userdetails.User
+                .withUsername(appUser.getUsername())
+                .password(appUser.getPassword())
+                .authorities(new SimpleGrantedAuthority(appUser.getRole()))
+                .build();
+    }
 }
